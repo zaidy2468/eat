d9954c2fe958fa000e81e31d0f78105eac145d9d
zaidy2468 <zaidyousaf98@gmail.com>
2023-09-22 01:45:17 +0500

    first commit for eat-curious frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      rotate:{
+        '17':'15deg'
+      },
+      fontWeight:{
+          'extra-bold':800,
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      bggradientmask: {
+        'gradient-radial':
+        'radial-gradient(34.99px at 50% calc(100% - 48.00px), #000 99%, #0000 101%) calc(50% - 60px) 0/120px 100%, radial-gradient(34.99px at 50% calc(100% + 18.00px), #0000 99%, #000 101%) 50% calc(100% - 30px)/120px 100% repeat-x',
+      },
+      
     },
   },
   plugins: [],
+  corePlugins:{
+    aspectRatio:true
+  },
+  
+
 }
